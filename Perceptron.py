87ee6c95f2dbd4ec0ce7c9932f9d241bf7798cf7
ys <teyandeymain1@gmail.com>
2026-06-24 14:27:08 +0000

    start docker

--- Perceptron.py
+++ Perceptron.py
@@ -1,8 +1,8 @@
-def and(x1,x2):
+def AND(x1,x2):
   w1,w2,theta = 0.5,0.5,0.7
   tmp = x1*w1 + x2*w2
-  if tmp <= theta
+  if tmp <= theta:
     return 0
-  else return 1
+  else:
+    return 1
 
-and (0,0)
